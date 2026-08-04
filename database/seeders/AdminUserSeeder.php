07c1342aff550016b66766cbd2bd38479e484b6a
Sebastian Calapsu <calapsusebastian@gmail.com>
2026-08-04 16:03:51 -0500

    Add dedicated admin user seeder for production bootstrap
    Separate from DatabaseSeeder/InventoryRecordSeeder so it can be run
    on production without touching real inventory data or creating the
    sample test users.

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'calapsusebastian@gmail.com'],
+            [
+                'name' => 'Sebastian Calapsu',
+                'password' => Hash::make('Damocles1/('),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
